73404f69e82376f6711939af28c0996e016ec6a8
hduarte <29019005+Bacmel@users.noreply.github.com>
2022-07-04 15:53:09 +0200

    feat: :sparkles: Add New Robot State Machine

---
+++ odri_interface/include/robot_state_machine.hpp
@@ -0,0 +1,137 @@
+#pragma once
+
+#include <chrono>
+#include <memory>
+#include <vector>
+
+#include <Eigen/Dense>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "odri_msgs/msg/driver_state.hpp"
+#include "odri_msgs/msg/master_board_state.hpp"
+#include "odri_msgs/msg/robot_command.hpp"
+#include "odri_msgs/msg/robot_state.hpp"
+#include "odri_msgs/srv/transition_command.hpp"
+
+#include "odri_control_interface/robot.hpp"
+#include "odri_control_interface/utils.hpp"
+
+namespace odri_ros2_interface
+{
+
+enum class StateType
+{
+    Idle,
+    Calibrating,
+    Enabled,
+    Running,
+    NbStates
+};
+enum class TransitionType
+{
+    Enable,
+    Calibrate,
+    Start,
+    Disable,
+    Stop,
+    NbTransitions
+};
+
+static std::map<TransitionType, std::pair<StateType, StateType>> transitionStates_init_map()
+{
+    std::map<TransitionType, std::pair<StateType, StateType>> m;
+    m.clear();
+    m.insert({TransitionType::Enable, {StateType::Idle, StateType::Calibrating}});
+    m.insert({TransitionType::Enable, {StateType::Calibrating, StateType::Enabled}});
+    m.insert({TransitionType::Start, {StateType::Enabled, StateType::Running}});
+    m.insert({TransitionType::Disable, {StateType::Enabled, StateType::Idle}});
+    m.insert({TransitionType::Stop, {StateType::Running, StateType::Idle}});
+
+    return m;
+}
+
+static std::map<StateType, std::string> stateName_init_map()
+{
+    std::map<StateType, std::string> m;
+    m.clear();
+    m.insert({StateType::Idle, "IDLE"});
+    m.insert({StateType::Calibrating, "CALIBRATING"});
+    m.insert({StateType::Enabled, "ENABLED"});
+    m.insert({StateType::Running, "RUNNING"});
+
+    return m;
+}
+
+static std::map<std::string, TransitionType> nameTransition_init_map()
+{
+    std::map<std::string, TransitionType> m;
+    m.clear();
+    m.insert({"enable", TransitionType::Enable});
+    m.insert({"calibrate", TransitionType::Calibrate});
+    m.insert({"start", TransitionType::Start});
+    m.insert({"disable", TransitionType::Disable});
+    m.insert({"stop", TransitionType::Stop});
+
+    return m;
+}
+
+// This static variable stores which states a transition relates. origin and destination are stored as std pair
+static const std::map<TransitionType, std::pair<StateType, StateType>> transition_states_map =
+    transitionStates_init_map();
+static const std::map<StateType, std::string>      state_name_map      = stateName_init_map();
+static const std::map<std::string, TransitionType> name_transition_map = nameTransition_init_map();
+
+class RobotStateMachine : public rclcpp::Node
+{
+  public:
+    explicit RobotStateMachine(const std::string &node_name);
+    virtual ~RobotStateMachine();
+
+  private:
+    void declareParameters();
+    void createOdriRobot(const std::string &robot_yaml_path);
+
+    void callbackTimerSendCommand();
+    void callbackRobotCommand(const odri_msgs::msg::RobotCommand::SharedPtr msg);
+
+    void transitionRequest(const std::shared_ptr<odri_msgs::srv::TransitionCommand::Request>  request,
+                           const std::shared_ptr<odri_msgs::srv::TransitionCommand::Response> response);
+
+    bool smDisable();
+    bool smCalibrate();
+    bool smEnable();
+    bool smStart();
+    bool smStop();
+
+  private:
+    rclcpp::Publisher<odri_msgs::msg::RobotState>::SharedPtr      pub_robot_state_;
+    rclcpp::Subscription<odri_msgs::msg::RobotCommand>::SharedPtr sub_motor_command_;
+    rclcpp::Service<odri_msgs::srv::TransitionCommand>::SharedPtr srv_sm_transition_;
+
+    std::shared_ptr<odri_control_interface::Robot> odri_robot_;
+
+    odri_msgs::msg::RobotState robot_state_msg_;
+
+    Eigen::VectorXd positions_;
+    Eigen::VectorXd velocities_;
+    Eigen::VectorXd torques_;
+
+    Eigen::VectorXd des_torques_;
+    Eigen::VectorXd des_positions_;
+    Eigen::VectorXd des_velocities_;
+    Eigen::VectorXd des_pos_gains_;
+    Eigen::VectorXd des_vel_gains_;
+    Eigen::VectorXd max_currents_;
+
+    struct Params
+    {
+        std::string robot_yaml_path;
+        Eigen::VectorXd safety_damping;
+        Eigen::VectorXd safety_position;
+    } params_;
+
+    StateType current_state_;
+};
+
+}  // namespace odri_ros2_interface
---
+++ odri_interface/src/robot_state_machine.cpp
@@ -0,0 +1,272 @@
+#include "odri_interface/robot_state_machine.hpp"
+
+namespace odri_ros2_interface
+{
+RobotStateMachine::RobotStateMachine(const std::string &node_name) : rclcpp::Node(node_name)
+{
+    declareParameters();
+    createOdriRobot(params_.robot_yaml_path);
+
+    pub_robot_state_ = create_publisher<odri_msgs::msg::RobotState>("robot_state", 1);
+
+    sub_motor_command_ = create_subscription<odri_msgs::msg::RobotCommand>(
+        "robot_command", rclcpp::QoS(1),
+        std::bind(&RobotStateMachine::callbackRobotCommand, this, std::placeholders::_1));
+
+    srv_sm_transition_ = create_service<odri_msgs::srv::TransitionCommand>(
+        std::string(std::string(get_name()) + "/state_transition").c_str(),
+        std::bind(&RobotStateMachine::transitionRequest, this, std::placeholders::_1, std::placeholders::_2),
+        rmw_qos_profile_services_default);
+
+    timer_send_commands_ = create_wall_timer(std::chrono::duration<double, std::milli>(1),
+                                             std::bind(&RobotStateMachine::callbackTimerSendCommands, this));
+
+    positions_  = Eigen::VectorXd::Zero(odri_robot_->joints->GetNumberMotors());
+    velocities_ = positions_;
+    torques_    = positions_;
+
+    des_torques_    = positions_;
+    des_positions_  = positions_;
+    des_velocities_ = positions_;
+    des_pos_gains_  = positions_;
+    des_vel_gains_  = positions_;
+    max_currents_   = positions_;
+
+    current_state_ = StateType::Idle;
+}
+
+RobotStateMachine::~RobotStateMachine() {}
+
+void RobotStateMachine::declareParameters()
+{
+    declare_parameter<std::string>("robot_yaml_path", "");
+    get_parameter<std::string>("robot_yaml_path", params_.robot_yaml_path);
+    declare_parameter<std::string>("safety_position", "");
+    get_parameter<std::string>("safety_position", params_.safety_position);
+    declare_parameter<std::string>("safety_damping", "");
+    get_parameter<std::string>("safety_damping", params_.safety_damping);
+}
+
+void RobotStateMachine::createOdriRobot(const std::string &robot_yaml_path)
+{
+    odri_robot_ = odri_control_interface::RobotFromYamlFile(robot_yaml_path);
+    odri_robot_->Start();
+    odri_robot_->WaitUntilReady();
+}
+
+void RobotInterface::callbackTimerSendCommands()
+{
+    odri_robot_->ParseSensorData();
+
+    positions_  = odri_robot_->joints->GetPositions();
+    velocities_ = odri_robot_->joints->GetVelocities();
+    torques_    = odri_robot_->joints->GetMeasuredTorques();
+
+    robot_state_msg_.header.stamp = get_clock()->now();
+    robot_state_msg_.motor_states.clear();
+
+    for (std::size_t i = 0; i < positions_.size(); ++i)
+    {
+        odri_msgs::msg::MotorState m_state;
+
+        m_state.position = positions_(i);
+        m_state.velocity = velocities_(i);
+        m_state.torque   = torques_(i);
+
+        robot_state_msg_.motor_states.push_back(m_state);
+    }
+    robot_state_msg_.sm_state = state_name_map.at(current_state_);
+    pub_robot_state_->publish(robot_state_msg_);
+
+    switch (current_state_)
+    {
+        case StateType::Running:
+            odri_robot_->joints->SetTorques(des_torques_);
+            odri_robot_->joints->SetDesiredPositions(des_positions_);
+            odri_robot_->joints->SetDesiredVelocities(des_velocities_);
+            odri_robot_->joints->SetPositionGains(des_pos_gains_);
+            odri_robot_->joints->SetVelocityGains(des_vel_gains_);
+            odri_robot_->joints->SetMaximumCurrents(
+                max_currents_(0));  // WARNING: Max current is common for all joints
+            break;
+
+        case StateType::Enabled:
+            Eigen::VectorXd vec_zero = Eigen::VectorXd::Zero(odri_robot_->GetJoints()->GetNumberMotors());
+            odri_robot_->joints->SetTorques(vec_zero);
+            odri_robot_->joints->SetDesiredPositions(params_.safety_position);
+            odri_robot_->joints->SetDesiredVelocities(vec_zero);
+            odri_robot_->joints->SetPositionGains(params_.safety_damping);
+            odri_robot_->joints->SetVelocityGains(params_.safety_damping);
+            break;
+
+        default:
+            Eigen::VectorXd vec_zero = Eigen::VectorXd::Zero(odri_robot_->GetJoints()->GetNumberMotors());
+            odri_robot_->joints->SetTorques(vec_zero);
+            odri_robot_->joints->SetDesiredPositions(vec_zero);
+            odri_robot_->joints->SetDesiredVelocities(vec_zero);
+            odri_robot_->joints->SetPositionGains(vec_zero);
+            odri_robot_->joints->SetVelocityGains(vec_zero);
+            break;
+    }
+
+    if (!odri_robot_->SendCommand())
+    {
+        odri_robot_->ReportError();
+        RCLCPP_INFO_STREAM(get_logger(), "\nError detected: ");
+        if (smStop())
+        {
+            RCLCPP_INFO_STREAM(get_logger(), "odri go to ENABLED state.")
+        }
+        else
+        {
+            smDisable();
+            RCLCPP_INFO_STREAM(get_logger(), "odri go to DISABLED state.");
+        }
+    }
+}
+
+void RobotStateMachine::callbackRobotCommand(const odri_msgs::msg::RobotCommand::SharedPtr msg)
+{
+    if (current_state_ == StateType::Running)
+    {
+        for (std::size_t i = 0; i < msg->motor_commands.size(); ++i)
+        {
+            des_torques_(i)    = msg->motor_commands[i].torque_ref;
+            des_positions_(i)  = msg->motor_commands[i].position_ref;
+            des_velocities_(i) = msg->motor_commands[i].velocity_ref;
+            des_pos_gains_(i)  = msg->motor_commands[i].kp;
+            des_vel_gains_(i)  = msg->motor_commands[i].kd;
+            max_currents_(i)   = msg->motor_commands[i].i_sat;
+        }
+    }
+}
+
+void RobotStateMachine::transitionRequest(const std::shared_ptr<odri_msgs::srv::TransitionCommand::Request>  request,
+                                          const std::shared_ptr<odri_msgs::srv::TransitionCommand::Response> response)
+{
+    RCLCPP_INFO_STREAM(get_logger(), "Service request received");
+
+    TransitionType command = TransitionType::NbTransitions;
+    if (name_transition_map.find(request->command) != name_transition_map.end())
+    {
+        command = name_transition_map.at(request->command);
+    }
+
+    switch (command)
+    {
+        case TransitionType::Enable:
+            response->accepted = smEnable();
+            break;
+        case TransitionType::Start:
+            response->accepted = smStart();
+            break;
+        case TransitionType::Disable:
+            response->accepted = smDisable();
+            break;
+        case TransitionType::Stop:
+            response->accepted = smStop();
+            break;
+        default:
+            response->accepted = false;
+            response->message  = "Command: " + request->command +
+                                " does not exist. Possible options are: 'enable'|'disable'|'calibrate'|'start'|'stop'";
+            RCLCPP_WARN_STREAM(get_logger(), response->message);
+            break;
+    }
+    if (response->accepted)
+    {
+        response->message = "Command: " + request->command + " accepted.";
+    }
+    else
+    {
+        response->message = "Command: " + request->command + " not accepted.";
+    }
+    response->result = state_name_map.at(current_state_);
+    RCLCPP_WARN_STREAM(get_logger(), response->message);
+}
+
+bool RobotStateMachine::smDisable()
+{
+    odri_robot_->ReportError();
+    createOdriRobot(params_.robot_yaml_path);
+    current_state_ = StateType::Idle;
+    return true;
+}
+
+bool RobotStateMachine::smCalibrate()
+{
+    if (current_state_ == StateType::Idle)
+    {
+        Eigen::VectorXd zero_vec = Eigen::VectorXd::Zero(odri_robot_->GetJoints()->GetNumberMotors());
+        odri_robot_->GetJoints()->SetPositionOffsets(zero_vec);
+
+        RCLCPP_INFO_STREAM(get_logger(), "\nCalibration procedure: Finding indexes...");
+        odri_robot_->RunCalibration(zero_vec);
+
+        RCLCPP_INFO_STREAM(get_logger(),
+                           "\nCalibration procedure: Put all joints in their respective zero positions. Call state "
+                           "transition service with 'enable'");
+
+        current_state_ = StateType::Calibrating;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool RobotStateMachine::smEnable()
+{
+    if (current_state_ == StateType::Idle)
+    {
+        RCLCPP_INFO_STREAM(get_logger(), "\nThese are the offsets");
+        Eigen::VectorXd current_pos = odri_robot_->GetJoints()->GetPositions();
+
+        for (size_t i = 0; i < current_pos.size(); i++)
+        {
+            RCLCPP_INFO_STREAM(get_logger(), "Joint " << i << ": " << current_pos(i));
+        }
+
+        odri_robot_->GetJoints()->SetPositionOffsets(-current_pos);
+
+        RCLCPP_INFO_STREAM(get_logger(), "Calibration done");
+
+        current_state_ = StateType::Enabled;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool RobotStateMachine::smStart()
+{
+    if (current_state_ == StateType::Enabled)
+    {
+        current_state_ = StateType::Running;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool RobotStateMachine::smStop()
+{
+    if (current_state_ == StateType::Running)
+    {
+        current_state_ = StateType::Enabled;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void
+
+}  // namespace odri_ros2_interface
