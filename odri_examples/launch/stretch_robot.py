b8e32c7c17f58f847bd3e501ad473571bf987b36
hduarte <29019005+Bacmel@users.noreply.github.com>
2022-07-04 09:00:56 +0200

    feat: :sparkles: Add stretch example

---
+++ odri_examples/launch/stretch_robot.py
@@ -0,0 +1,33 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    odri_iface_config = os.path.join(get_package_share_directory('odri_interface'), 'config', 'stretch_params.yaml')
+
+    odri_iface_node = Node(package='odri_interface',
+                           name='robot_interface',
+                           executable='robot_interface',
+                           parameters=[odri_iface_config],
+                           output='screen',
+                           remappings=[("robot_state", "/odri/robot_state"), ("robot_command", "/odri/robot_command")])
+
+    odri_stretch_node = Node(package='odri_examples',
+                             name='stretch_robot',
+                             executable='stretch_robot',
+                             output='screen',
+                             remappings=[("robot_state", "/odri/robot_state"),
+                                         ("robot_command", "/odri/robot_command")])
+
+    reconfigure = Node(package='rqt_reconfigure', name='rqt_reconfigure', executable='rqt_reconfigure')
+
+    ld.add_action(odri_iface_node)
+    ld.add_action(odri_stretch_node)
+    ld.add_action(reconfigure)
+
+    return ld
